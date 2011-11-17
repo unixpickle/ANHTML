3d358625cd918d55035a9d479f1bb8a4980e50e2
Alex Nichol <alexnichol@comcast.net>
2011-11-17 18:29:54 -0500

    Initial Commit

---
+++ ANHTML/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  ANHTML
+//
+//  Created by Alex Nichol on 11/17/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main (int argc, const char * argv[])
+{
+
+	@autoreleasepool {
+	    
+	    // insert code here...
+	    NSLog(@"Hello, World!");
+	    
+	}
+    return 0;
+}
+
